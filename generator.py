84b48a8c559440c8e488dd879c748f78db51cb88
Marc Sánchez <sanchezfauste@gmail.com>
2017-04-12 23:29:11 +0200

    Adding checks to problem generator

--- generator.py
+++ generator.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 
 import sys, random
+import os
 
 problems_folder="problems"
 
+if len(sys.argv) != 3:
+    print "Usage:", sys.argv[0], "<nitems> <weight>"
+    exit(1)
+
 nitems = int(sys.argv[1])
 weight = int(sys.argv[2])
+
+if (not os.path.exists(problems_folder)):
+    print "ERROR: directory", problems_folder, "does not exists!"
+    exit(1)
 
 f = open(problems_folder + '/' + str(nitems) + '_' + str(weight), 'w')
 
