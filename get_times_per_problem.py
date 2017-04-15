8ce1bb5562457c102c54c4a9c0e36afee1b7aef8
Marc Sánchez <sanchezfauste@gmail.com>
2017-04-15 13:16:54 +0200

    Adding script to get times depending on cores for each problem

---
+++ get_times_per_problem.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+
+f = open(sys.argv[1], 'r')
+
+results = {}
+
+for line in f:
+   l = line.split()
+   if len(l) == 6:
+       results[(l[0], l[1], l[5])] = l[4]
+
+f.close()
+f = open(sys.argv[1], 'r')
+
+print "Width Items Time1Thread Time2Threads Time3Threads Time4Threads"
+for line in f:
+   line = line.strip('\n')
+   l = line.split()
+   try:
+       if len(l) == 6:
+           if l[5] == '1':
+               print l[0], l[1], l[4], results[(l[0], l[1], '2')], results[(l[0], l[1], '3')], results[(l[0], l[1], '4')]
+   except:
+       pass
+
+f.close()
